1a55aa63f004f159e0603aa3679a85bce1070574
U00000000Nya <41243128@nfu.edu.tw>
2024-10-16 22:41:53 +0900

    aaa

--- hw10161.cpp
+++ hw10161.cpp
@@ -1,37 +1,47 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include<string>
 using namespace std;
-int main() {
 
-    int n;
-    char str[999] = { 0 };
-    int count[91] = { 0 };
-    int i, j;
+int main ()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int i, j;
+	int n, a, l = 0;
+	int code[26]={};
+	
+	cin >> n;
+	
+	cin.ignore();
+	
+	string w;
+	
+	for (i=0;i<n;i++) {
+		getline(cin, w);
+		
+		for (j=0;j<w.size();j++) {
+			a = -1;
+			
+			if (w[j] >= 'a' && w[j] <= 'z')
+				a = w[j] - 'a';
+			else if (w[j] >= 'A' && w[j] <= 'Z')
+				a = w[j] - 'A';
+			
+			if (a != -1) {
+				l++;
+				code[a]++;
+			}
+		}
+	}
+	
+	for (j=l;j>=1;j--) {
+		for (i=0;i<26;i++) {
+			if (code[i] == j) {
+				cout << (char) (i + 'A') << " " << code[i] << endl;
+			}
+		}
+	}
 
-    cin >> n;
-    getchar();
-
-    while (n--) {
-
-        gets(str);
-
-        for (i = 0; i < strlen(str); i++) {
-            if (str[i] >= 'a' && str[i] <= 'z') {
-                count[str[i] - 'a' + 'A']++;
-            }
-            else {
-                count[str[i]]++;
-            }
-        }
-    }
-
-    for (i = sizeof(str); i >= 1; i--) {
-        for (j = 'A'; j <= 'Z'; j++) {
-            if (count[j] == i) {
-                cout <<(char)j<< " " << i << endl;
-            }
-        }
-    }
-
-    return 0;
+	return 0;
 }
