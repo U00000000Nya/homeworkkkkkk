b352f55c6fe227cd3707f2f662cc8d46a7acff0e
徐聖硯 Hsu Sheng Yen <41243128@nfu.edu.tw>
2024-10-22 18:55:48 +0900

    Update 10231.cpp

--- 1023homework/10231.cpp
+++ 1023homework/10231.cpp
@@ -2,13 +2,13 @@
 using  namespace std;
 int ackermann(int m, int n)
 {
-	if (m == 0)
+	if (m == 0) //M=0的情況
 	{
 		return n + 1;
 	}
-	else if (n == 0)
+	else if (n == 0) //N=0的情況
 	{
-		return ackermann(m - 1, 1);
+		return ackermann(m - 1, 1); 
 	}
 	else
 	{
