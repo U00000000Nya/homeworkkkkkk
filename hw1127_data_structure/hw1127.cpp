0f14f380618b176f527b88cf6b968560f462e14d
徐聖硯 <41243128@nfu.edu.tw>
2024-12-09 23:10:44 +0900

    Update hw1127.cpp
    okkkkkkkk

--- hw1127_data_structure/hw1127.cpp
+++ hw1127_data_structure/hw1127.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 class Term {
     friend class Polynomial; // Polynomial 可存取 Term 的私有成員
+    friend ostream& operator<<(ostream& output, const Polynomial& p);
 private:
     float coef; // 係數
     int exp;    // 指數
 };
 
 class Polynomial {
+    friend istream& operator>>(istream& input, Polynomial& p);
+    friend ostream& operator<<(ostream& output, const Polynomial& p);
 public:
     // 建構子
     Polynomial() : termArray(nullptr), capacity(0), terms(0) {}
@@ -22,40 +25,10 @@ public:
     }
 
     ~Polynomial() {
-        
-    }
-
-    // 輸入多項式
-    void Input() {
-        cout << "輸入多項式(格式:3x^2 4x 5,每項用空格分隔，結束輸入按 Enter):";
-        while (true) {
-            float coef;
-            int exp;
-            char variable, caret;
-            cin >> coef;
-            if (cin.peek() == '\n' || !cin) {
-                AddTerm(coef, 0);
-                break;
-            }
-            cin >> variable; // 讀取 'x'
-            if (variable == 'x') {
-                if (cin.peek() == '^') {
-                    cin >> caret >> exp; // 讀取 '^' 和指數
-                }
-                else {
-                    exp = 1; // 如果沒有 ^，預設指數為 1
-                }
-            }
-            else {
-                exp = 0; // 如果沒有 x，指數為 0
-            }
-            AddTerm(coef, exp);
-            if (cin.peek() == '\n') break; // 如果換行符則結束
-        }
     }
 
     // 多項式相加
-    Polynomial Add(Polynomial poly) {
+    Polynomial Add(const Polynomial& poly) {
         Polynomial result(max(capacity, poly.capacity));
         int i = 0, j = 0;
         while (i < terms && j < poly.terms) {
@@ -86,7 +59,7 @@ public:
     }
 
     // 多項式相乘
-    Polynomial Mult(Polynomial poly) {
+    Polynomial Mult(const Polynomial& poly) {
         Polynomial result(terms * poly.terms);
         for (int i = 0; i < terms; i++) {
             for (int j = 0; j < poly.terms; j++) {
@@ -106,27 +79,11 @@ public:
         }
         return result;
     }
-
-    // 輸出多項式
-    void Print() const {
-        if (terms == 0) {
-            cout << "0" << endl;
-            return;
-        }
-        for (int i = 0; i < terms; i++) {
-            if (i > 0 && termArray[i].coef > 0) cout << " + ";
-            cout << termArray[i].coef;
-            if (termArray[i].exp != 0) cout << "x^" << termArray[i].exp;
-        }
-        cout << endl;
-    }
-
 private:
-    Term* termArray;
-    int capacity;
-    int terms;
+    Term* termArray; // 儲存多項式的項目
+    int capacity;    // 容量
+    int terms;       // 當前項數
 
-    // 新增項目
     void AddTerm(float coef, int exp) {
         if (coef == 0) return;
         if (terms >= capacity) {
@@ -137,7 +94,6 @@ private:
         terms++;
     }
 
-    // 複製陣列並調整容量
     void Copy(int newCapacity) {
         Term* newArray = new Term[newCapacity];
         copy(termArray, termArray + terms, newArray);
@@ -146,7 +102,6 @@ private:
         capacity = newCapacity;
     }
 
-    // 新增項目到結果多項式
     void AddTermToResult(float coef, int exp) {
         for (int i = 0; i < terms; i++) {
             if (termArray[i].exp == exp) {
@@ -157,39 +112,85 @@ private:
         AddTerm(coef, exp);
     }
 };
+ostream& operator<<(ostream& output, const Polynomial& p) {
+    if (p.terms == 0) {
+        output << "0";
+        return output;
+    }
+    for (int i = 0; i < p.terms; i++) {
+        if (i > 0 && p.termArray[i].coef > 0) output << " + ";
+        output << p.termArray[i].coef;
+        if (p.termArray[i].exp != 0) output << "x^" << p.termArray[i].exp;
+    }
+    return output;
+}
+istream& operator>>(istream& input, Polynomial& p) {
+    float t_coef;
+    int t_exp;
+    char tmp;
+    bool isNegative = false;
 
+    while (true) {
+        input >> t_coef; // 讀入係數
+        if (isNegative) {
+            t_coef *= -1; // 處理負號
+            isNegative = false;
+        }
+
+        input.get(tmp); // 讀取下一個字元，可能是 'x' 或 '\n'
+        if (tmp == '\n') {
+            p.AddTerm(t_coef, 0); // 如果是 '\n'，代表是常數
+            break;
+        }
+
+        if (tmp == 'x') {
+            input.get(tmp); // 讀取 '^' 或 '\n'
+            if (tmp == '^') {
+                input >> t_exp; // 讀取指數
+            }
+            else {
+                t_exp = 1; // 預設指數為 1
+                if (tmp == '\n') break;
+            }
+        }
+        else {
+            t_exp = 0; // 沒有 'x'，指數為 0
+        }
+
+        p.AddTerm(t_coef, t_exp); // 新增項目
+
+        input.get(tmp); // 讀取下一個項目的分隔符號
+        if (tmp == '\n') break; // 若為換行符，結束輸入
+        else if (tmp == '-') isNegative = true; // 若為負號，處理下一項目
+    }
+    return input;
+}
 int main() {
     Polynomial p1(10), p2(10);
 
-    // 輸入第一個多項式
-    cout << "第一個多項式" << endl;
-    p1.Input();
+    // 輸入多項式
+    cout << "輸入第一個多項式 (格式：3x^2 -4x +5):";
+    cin >> p1;
+    cout << "輸入第二個多項式 (格式：2x^3 +x -7):";
+    cin >> p2;
 
-    // 輸入第二個多項式
-    cout << "第二個多項式" << endl;
-    p2.Input();
-
-    cout << "多項式 P1 = ";
-    p1.Print();
-
-    cout << "多項式 P2 = ";
-    p2.Print();
+    // 輸出多項式
+    cout << "多項式 P1 = " << p1 << endl;
+    cout << "多項式 P2 = " << p2 << endl;
 
     // 多項式相加
     Polynomial sum = p1.Add(p2);
-    cout << "P1 + P2 = ";
-    sum.Print();
+    cout << "P1 + P2 = " << sum << endl;
 
     // 多項式相乘
     Polynomial product = p1.Mult(p2);
-    cout << "P1 * P2 = ";
-    product.Print();
+    cout << "P1 * P2 = " << product << endl;
 
     // 計算多項式值
     float x;
     cout << "輸入 x 的值來計算 P1(x): ";
     cin >> x;
-    cout << "P1(X=" << x << ") 的值 = " << p1.Eval(x) << endl;
+    cout << "P1(x=" << x << ") 的值 = " << p1.Eval(x) << endl;
 
     return 0;
 }
